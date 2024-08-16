56db9c86eabc99e0c0876f1df39a840d51b81d34
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-08-16 09:44:20 -0700

    Fixed a bug in rendering the shape

--- index.js
+++ index.js
@@ -2,33 +2,29 @@
 const inquirer = require("inquirer");
 const { join } = require("path");
 const fs = require('fs');
-const { writeFile } = require('fs/promises');
 //Imports the shapes module
 const { Circle, Square, Triangle } = require("./lib/shapes");
 
 //Define a SVG class that has a constructor that takes two arguments: text and shape.
-class Svg {
-  //Constructor
-  //Text and shape elements are set to empty strings
-  //Text element is set to the text argument and the color argument
-  //Shape element is set to the shape argument and the color argument
-  constructor() {
-    this.textElement = "";
-    this.shapeElement = "";
+class Svg{
+  constructor(){
+      this.textElement = ''
+      this.shapeElement = ''
   }
-  //Render method returns the SVG string with the text and shape elements
-  render() {
-    `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+  render(){
+
+      return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
   }
-  //Set text method sets the text element
-  setTextElement(text, color) {
-    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  setTextElement(text,color){
+      this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
   }
-  //Set shape method sets the shape element
-  setShapeElement(shape) {
-    this.shapeElement = shape.render();
+  setShapeElement(shape){
+      this.shapeElement = shape.render()
+
   }
+  
 }
+
 //Array of questions for user input using inquirer prompt
 const questions = [
   //Text input
@@ -120,7 +116,9 @@ async function init() {
     //create a new SVG instance
     var svg = new Svg();
     svg.setTextElement(user_text, user_textColor);
+    console.log(svg);
     svg.setShapeElement(user_shape);
+    console.log(svg);
     svgString = svg.render();
     console.log(svgString);
     //write the SVG string to a file

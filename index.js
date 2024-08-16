cb411cdc60976d1e48b3ffed16f2719f0286728c
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-08-15 20:22:03 -0700

    Modified the index.js to include the SVG class with a constructor and added render method

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-// Imports the fs, inquirer, Circle, Square, and Triangle modules.
+// Imports the fs, inquirer and path
 const inquirer = require('inquirer');
 const { join } = require('path');
 const { writeFile } = require('fs/promises');
 //Imports the shapes module
 const {Circle, Square, Triangle} = require("./lib/shapes");
 
-//Define a SVG class that has a constructor that takes three arguments: text, textColor, and shape.
+//Define a SVG class that has a constructor that takes two arguments: text and shape.
 class SVG {
+    //Constructor
+    //Text and shape elements are set to empty strings
+    //Text element is set to the text argument and the color argument
+    //Shape element is set to the shape argument and the color argument
     constructor() {
-        this.text = "";
-        this.textColor = "";
-        this.shape = "";
+        this.textElement = "";
+        this.shapeElement = "";
     }
+    //Render method returns the SVG string with the text and shape elements
     render() {
-        throw new Error("Child class must implement a render() method.");
+        `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
     }
-    setText(text, textColor) {
-        this.text = text;
-        this.textColor = textColor;
+    //Set text method sets the text element
+    setText(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
     }
+    //Set shape method sets the shape element
     setShape(shape) {
-        this.shape = shape;
+        this.shapeElement = shape.render()
     }
 }
-
-//Define a SVGText class that extends the SVG class and has a constructor that takes two arguments: text and textColor.
